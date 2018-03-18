98d360bd4c588c139dd4418abaa73be3c71394fd
Tapszvidzwa <zvidzwat@grinnell.edu>
2018-03-17 23:18:32 -0500

    Get an element that is n places from the back of the list

---
+++ GetNode.c
@@ -0,0 +1,105 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+//node struct
+typedef struct node{
+	int data;
+	struct node* next;
+} node_t;
+
+//
+typedef struct list{
+	struct node* head;
+} list_t;
+
+
+node_t* create_node(int n){
+	node_t* new_node = (node_t*) malloc(sizeof(node_t));
+	new_node->data = n;
+	new_node->next = NULL;
+	return new_node;
+}
+
+void insert(node_t* node, list_t* lst) {
+
+	//if list is null
+	if(lst->head == NULL) {
+		lst->head = node;
+	} else {
+
+    //traverse and add to the end
+	node_t* current = lst->head;
+	while(current->next != NULL) {
+		current = current->next;
+		}
+
+	current->next = node;
+	}
+
+}
+
+
+//Get the node at index n from the back of the list
+//
+//Comments
+// Turns out that this is a brute force solution which traverses the linked list twice
+// The time complexity is just O(n) 
+
+// However another clever implementaiton is to create a poitner that will point n nodes behind the
+// current pointer
+
+// SUppose u have a list with 5 elements and you want the second element from the last in the list,
+// you iterate through the list and when you get to element 2 in your iteration you activate the 
+// the result pointer to point to element 0 and it will start also iterating as you continue iterating the list
+int getNode(node_t* head, int n) {
+
+node_t* cur = head;
+
+int total = 0;
+
+while(cur != NULL) {
+	++total;
+	cur = cur->next;
+}
+
+cur = head;
+
+int steps = total - n;
+
+for(int i = 0; i < steps - 1; i++) {
+	cur = cur->next;
+}
+
+return cur->data;
+
+}
+
+
+int main() {
+
+ list_t* list = (list_t*) malloc(sizeof(list_t));
+ list->head = NULL;
+ int list_len = 0;
+
+ srand(time(0));
+
+ //get the input from the user until they press 'n'
+ 	int c;
+ 	while((c = getchar())!= '\n') {
+ 		node_t* new_node = create_node(c);
+ 		insert(new_node, list);
+ 		++list_len;
+ 	}
+
+ 	node_t* current = list->head;
+
+ int random = rand() % list_len;
+
+ printf("\n PRINTING ELEMENT NUMBER: %d\n", random);
+ printf("%c\n", (char) getNode(list->head, random));
+
+}
+
+
